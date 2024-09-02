d90f93308f95af30cdd14055a6e54a484c1adbf3
hariom <hariomsh.jpr@gmail.com>
2024-09-02 17:39:20 +0530

    add these in commitlint feature file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ["@commitlint/cli", "@commitlint/config-conventional"],  "commitlint": Unknownword,
+    extends: ["@commitlint/cli", "@commitlint/config-conventional"],  
     rules: {
         "type-enum":[
             2,
